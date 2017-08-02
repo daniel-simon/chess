8695cfe3edb13a5e788c64393870e1e5d531aacf
daniel-simon <daniel.alexander.simon@gmail.com>
2017-08-02 00:06:12 -0400

    finalized games api controller + serializer

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::GamesController < ApplicationController
   def index
-    render json: { games: Game.all }
+    render json: Game.all, adapter: :json
   end
 end
--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,3 +1,3 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id, :creator_id, :public_game, :show_legal_moves, :started
+  attributes :id, :creator_id, :public_game, :started, :show_legal_moves, :active_player_id, :inactive_player_id
 end
