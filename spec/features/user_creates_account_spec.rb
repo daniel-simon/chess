86d8383f367e5274273fb8eb5fb1d546ce6f04fa
daniel-simon <daniel.alexander.simon@gmail.com>
2017-08-02 00:10:03 -0400

    expanded user login feature tests

--- spec/features/user_creates_account_spec.rb
+++ spec/features/user_creates_account_spec.rb
@@ -19,17 +19,45 @@ feature 'User registration' do
 
     click_on 'Sign up'
 
-    expect(page).to have_content('Account created successfully')
+    expect(page).to have_content('Account successfully created')
   end
 
-  scenario 'user does not fill all fields and submit fails' do
+  scenario "user doesn't fill in email and submit fails" do
     visit '/'
     click_on 'Sign up'
 
     fill_in 'Username', with: 'TestUser'
     fill_in 'Password', with: 'TestUserPassword'
+    fill_in 'Confirm Password', with: 'TestUserPassword'
 
-    click_button 'Sign up'
+    click_on 'Sign up'
+
+    expect(page).to have_content('Failed to create account')
+  end
+
+  scenario "user doesn't fill in username and submit fails" do
+    visit '/'
+    click_on 'Sign up'
+
+    fill_in 'Email', with: 'TestUser@launch.com'
+    fill_in 'Password', with: 'TestUserPassword'
+    fill_in 'Confirm Password', with: 'TestUserPassword'
+
+    click_on 'Sign up'
+
+    expect(page).to have_content('Failed to create account')
+  end
+
+  scenario "password and confirmation don't match" do
+    visit '/'
+    click_on 'Sign u'
+
+    fill_in 'Username', with: 'TestUser'
+    fill_in 'Email', with: 'TestUser@launch.com'
+    fill_in 'Password', with: 'I got my own '
+    fill_in 'Confirm Password', with: ' password wrong :('
+
+    click_on 'Sign up'
 
     expect(page).to have_content('Failed to create account')
   end
