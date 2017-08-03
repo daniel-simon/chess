3b08fd38bcabe3b7a7344412e3064a0e3ddb6ca6
daniel-simon <daniel.alexander.simon@gmail.com>
2017-08-03 15:14:00 -0400

    configured games api controller to serve up booleans indicating which games the current user is playing or created

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,7 +1,20 @@
 class Api::V1::GamesController < ApplicationController
   def index
     # for the games index page
-    games = Game.where(finished: false)
-    render json: games, adapter: :json
+
+    games = []
+    game_models = Game.where(finished: false)
+    game_models.each_with_index do |game_model, i|
+      games[i] = game_model.serializable_hash(only: [:id, :started, :show_legal_moves, :created_at, :creator_id, :joiner_id])
+      games[i]["my_game"] = false
+      games[i]["playing_this_game"] = false
+      if current_user.id == game_model.creator_id
+        games[i]["my_game"] = true
+        games[i]["playing_this_game"] = true
+      elsif current_user.id == game_model.joiner_id
+        games[i]["playing_this_game"] = true
+      end
+    end
+    render json: { games: games }, adapter: :json
   end
 end
