c3e8d4fac6d157483ce9d169a5ceb29402673ccc
daniel-simon <daniel.alexander.simon@gmail.com>
2017-08-04 11:29:11 -0400

    updated games api controller tests for its new functionality

--- spec/controllers/api/v1/games_controller_spec.rb
+++ spec/controllers/api/v1/games_controller_spec.rb
@@ -6,8 +6,13 @@ RSpec.describe Api::V1::GamesController, type: :controller do
     before(:each) do
       @bobby_f = FactoryGirl.create(:user, username: "bfischer")
       @john_a = FactoryGirl.create(:user, username: "jakers")
+      session[:user_id] = @bobby_f.id
       FactoryGirl.create(:game, creator: @bobby_f, show_legal_moves: false)
       FactoryGirl.create(:game, creator: @john_a)
+    end
+
+    after(:each) do
+      session[:user_id] = nil
     end
 
     it "should render a list of games with the correct length" do
@@ -31,6 +36,32 @@ RSpec.describe Api::V1::GamesController, type: :controller do
 
       expect(bobbys_game["creator_id"]).to eq @bobby_f.id
       expect(johns_game["creator_id"]).to eq @john_a.id
+    end
+
+    it "should render a list of games with the correct 'my_game' information" do
+      get :index
+      returned_json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq "application/json"
+
+      bobbys_game = returned_json["games"][0]
+      johns_game = returned_json["games"][1]
+
+      expect(bobbys_game["my_game"]).to eq true
+      expect(johns_game["my_game"]).to eq false
+    end
+
+    it "should render a list of games with the correct 'playing_this_game' information" do
+      get :index
+      returned_json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq "application/json"
+
+      bobbys_game = returned_json["games"][0]
+      johns_game = returned_json["games"][1]
+
+      expect(bobbys_game["playing_this_game"]).to eq true
+      expect(johns_game["playing_this_game"]).to eq false
     end
 
     it "should render a list of games with the correct :show_legal_moves information" do
