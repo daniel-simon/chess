0afe5115e2c14a7170922532ed06f1b8052f6650
daniel-simon <daniel.alexander.simon@gmail.com>
2017-08-08 00:50:04 -0400

    refactor for user authentication

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::GamesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    # for the games index page
+    # for games that arent over:
+    if current_user
+      user = current_user
+    else
+      return render status: 403
+    end
     games = []
     game_models = Game.where(finished: false)
     game_models.each_with_index do |game_model, i|
@@ -19,15 +24,15 @@ class Api::V1::GamesController < ApplicationController
       games[i]["my_game"] = false
       games[i]["playing_this_game"] = false
       games[i]["creator_name"] = User.find(game_model.creator_id).username
-      if current_user.id == game_model.creator_id
+      if user.id == game_model.creator_id
         games[i]["my_game"] = true
         games[i]["playing_this_game"] = true
-      elsif current_user.id == game_model.joiner_id
+      elsif user.id == game_model.joiner_id
         games[i]["playing_this_game"] = true
       end
       if games[i]["playing_this_game"]
         games[i]["my_turn"] = false
-        if game_model.active_player_id == current_user.id
+        if game_model.active_player_id == user.id
           games[i]["my_turn"] = true
         end
       end
@@ -36,29 +41,32 @@ class Api::V1::GamesController < ApplicationController
   end
 
   def show
+    if current_user
+      user = current_user
+    else
+      return render status: 403
+    end
     game_model = Game.find(game_id)
+    white = User.find(game_model.white_id)
+    black = User.find(game_model.black_id)
+    active_id = game_model.active_player_id
+    unless [white.id, black.id].include?(user.id)
+      return render status: 403
+    end
     game_data_hash = game_model.serializable_hash(
       except: [
         :active_player_id,
         :creator_id,
         :joiner_id,
         :winner_id,
-        :loser_id
+        :loser_id,
+        :created_at,
+        :updated_at
       ]
     )
-    player_ids = [game_model.white_id, game_model.black_id]
-    unless player_ids.include?(current_user.id)
-      return render status: 403
-    end
-    opponent_id = player_ids.each do |id|
-      break id unless current_user.id == id
-    end
+    opponent_id = (user.id == white.id ? black.id : white.id)
     opponent = User.find(opponent_id)
-    user = current_user
-    white = User.find(game_model.white_id)
-    black = User.find(game_model.black_id)
-    active_id = game_model.active_player_id
-    game_data_hash["player_data"] = {
+    game_data_hash[:player_data] = {
       user: {
         id: user.id,
         username: user.username,
@@ -71,17 +79,21 @@ class Api::V1::GamesController < ApplicationController
       },
       initial_active_player_label: (active_id == user.id ? 'user' : 'opponent')
     }
-
     render json: { game_show_data: game_data_hash }, adapter: :json
   end
 
   def update
+    if current_user
+      user = current_user
+    else
+      return render status: 403
+    end
     game_update_request_hash = JSON.parse(request.body.read)
     game = Game.find(game_id)
     case game_update_request_hash["patchType"]
     when "join-game"
       white_id = game.white_id
-      if game.update(joiner_id: current_user.id, started: true, active_player_id: white_id)
+      if game.update(joiner_id: user.id, started: true, active_player_id: white_id)
         render json: { joined_game: game.serializable_hash }, adapter: :json
       end
     when "switch-turns"
@@ -94,6 +106,11 @@ class Api::V1::GamesController < ApplicationController
   end
 
   def create
+    if current_user
+      user = current_user
+    else
+      return render status: 403
+    end
     create_game_request_hash = JSON.parse(request.body.read)
     show_legal_moves = create_game_request_hash["showLegalMoves"]
     new_game = Game.new(creator: current_user, show_legal_moves: show_legal_moves)
