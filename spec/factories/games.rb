a071055b16c8bb72e8864f0c88eabbff2e1560a2
daniel-simon <daniel.alexander.simon@gmail.com>
2017-08-08 20:27:48 -0400

    changes to seed data and code formatting

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :game do
     creator { FactoryGirl.create(:user) }
+    show_legal_moves { [true, false][rand 0..1] }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :user do
     username { Faker::Name.unique.first_name }
     email { Faker::Internet.unique.email }
-    password { Faker::Name.name }
+    password "password"
   end
 end
