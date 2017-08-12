c74122fa07789cc68bac7c3faf5164591b7630aa
daniel-simon <daniel.alexander.simon@gmail.com>
2017-08-11 23:37:19 -0400

    fixed bug causing infinite redirect on account creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      cookies.signed[:user_id] = @user.id
       session[:user_id] = @user.id
       flash[:notice] = "Account successfully created. Welcome, #{@user.username}!"
       redirect_to games_path
