45cf8f7cdf422121434a8e5e58cd2d55db6314fd
daniel-simon <daniel.alexander.simon@gmail.com>
2017-08-19 22:00:11 -0400

    added instance method to games model that returns all captured pieces of a given color

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,15 +2,23 @@ class Game < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
   belongs_to :joiner, class_name: "User", foreign_key: "joiner_id", optional: true
 
-  belongs_to :winner, class_name: "User", foreign_key: "winner_id", optional: true
-  belongs_to :loser, class_name: "User", foreign_key: "loser_id", optional: true
-
   belongs_to :white, class_name: "User", foreign_key: "white_id", optional: true
   belongs_to :black, class_name: "User", foreign_key: "black_id", optional: true
 
+  belongs_to :winner, class_name: "User", foreign_key: "winner_id", optional: true
+  belongs_to :loser, class_name: "User", foreign_key: "loser_id", optional: true
+
   has_many :moves
 
-  def total_moves
-    Move.where(game_id: :id).count
+  validates :creator_id, inclusion: { in: [:white_id, :black_id] }
+  validates :joiner_id, inclusion: { in: [:white_id, :black_id, nil] }
+
+  def captured_pieces(capturer_color)
+    capture_moves = self.moves.select do |move|
+      move.player_color == capturer_color &&
+      move.captured_piece != nil
+    end
+    captured_pieces = capture_moves.map { |move| move.captured_piece }
+    return captured_pieces
   end
 end
